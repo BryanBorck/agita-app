d28c9f6e5741fe8c6e43f1a8954d5f8529e38be5
Bryan <bryanborck@gmail.com>
2022-07-10 00:26:42 -0300

    front

--- frontend/src/components/Chatbot.js
+++ frontend/src/components/Chatbot.js
@@ -18,8 +18,8 @@ export default function Chatbot(props){
 
             <MainStyle>
                 <ButtonStyle onClick={() => window.open("http://localhost:5000/get")} >DENÚNCIA</ButtonStyle>
-                <ButtonStyle>DÚVIDAS</ButtonStyle>
-                <ButtonStyle>SUGESTÕES</ButtonStyle>
+                <ButtonStyle onClick={() => window.open("http://localhost:5000/get")}>DÚVIDAS</ButtonStyle>
+                <ButtonStyle onClick={() => window.open("http://localhost:5000/get")}>SUGESTÕES</ButtonStyle>
             </MainStyle>
 
             <PowerStyle src={PowerS} alt="" />
